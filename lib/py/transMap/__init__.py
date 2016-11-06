b96a34e737830eee407f5280b97446044ea63153
Mark Diekhans <markd@soe.ucsc.edu>
2016-11-06 15:44:28 -0800

    build cross ref and check psls with fasta sizes

--- lib/py/transMap/__init__.py
+++ lib/py/transMap/__init__.py
@@ -11,16 +11,32 @@ def getChromSizes(db):
     return "/hive/data/genomes/{db}/chrom.sizes".format(db=db)
 
 
-def parseTransMapId(transMapId):
+def parseAlignId(alignId):
     """parse a transmap id in the form srcDb:acc.ver-uniqmods... into a list of
-    (srcDb, acc.ver, uniqmods)
+    (srcDb, acc.ver, uniqmods).  This will parse a srouce or target alignment id
     """
-    m = re.match("^([a-zA-Z0-9]+):([A-Z0-9_]+\\.[0-9]+)-([0-9.]+)$", transMapId)
+    m = re.match("^([a-zA-Z0-9]+):([A-Z0-9_]+\\.[0-9]+)-([0-9.]+)$", alignId)
     if m is None:
-        raise Exception("can not parse transmap id \"{}\"".format(transMapId))
+        raise Exception("can not parse transmap id \"{}\"".format(alignId))
     return m.groups()
 
 
-def parseAccVer(accVer):
+def alignIdToSrcId(alignId):
+    "remove the unique suffix only, return srcId, including srcDb"
+    m = re.match("^([a-zA-Z0-9]+:[A-Z0-9_]+\\.[0-9]+)-([0-9.]+)$", alignId)
+    if m is None:
+        raise Exception("can not parse alignment id \"{}\"".format(alignId))
+    return m.group(1)
+
+
+def srcIdToAccv(srcId):
+    "convert a srcId to an accv"
+    m = re.match("^([a-zA-Z0-9]+):([A-Z0-9_]+\\.[0-9]+)$", srcId)
+    if m is None:
+        raise Exception("can not parse source id \"{}\"".format(srcId))
+    return m.group(2)
+
+
+def parseAccVer(accver):
     """parse acc.ver into (acc, ver)"""
-    return accVer.split(".")
+    return accver.split(".")
--- lib/py/transMap/transMapLite.py
+++ lib/py/transMap/transMapLite.py
@@ -3,37 +3,37 @@ sqllite3 databases objects for transmap intermediate data.
 """
 
 from collections import namedtuple
-from pycbio.hgdata.hgLite import HgLiteTable, PslLite
-from transMap import parseTransMapId
+from pycbio.hgdata.hgLite import HgLiteTable
 
 
 class SourceDbTables(object):
     """tables names in a source dataset sqlite3 database"""
     srcAlignsTbl = "srcAligns"
+    srcXRefTbl = "srcXRefs"
     srcMetaDataTbl = "srcMetaData"
     srcSeqTbl = "srcSeqs"
 
 
-class TransMapGene(namedtuple("TransMapGene", ("id", "cds", "db", "geneName", "geneId"))):
+class TransMapSrcGene(namedtuple("TransMapSrcGene", ("srcId", "accv", "cds", "geneName", "geneId"))):
     """metedata on gene or mRNA """
     __slots__ = ()
 
 
-class TransMapGeneLite(HgLiteTable):
+class TransMapSrcGeneLite(HgLiteTable):
     """
     Source gene information
     """
     __createSql = """CREATE TABLE {table} (
-            id text not null,
+            srcId text not null,
+            accv text not null,
             cds text,
-            db text not null,
             geneName text,
             geneId text);"""
-    __insertSql = """INSERT INTO {table} (id, cds, db, geneName, geneId) VALUES (?, ?, ?, ?, ?);"""
-    __indexSql = """CREATE UNIQUE INDEX {table}_id on {table} (id);"""
+    __insertSql = """INSERT INTO {table} (srcId, accv, cds, geneName, geneId) VALUES (?, ?, ?, ?, ?);"""
+    __indexSql = """CREATE UNIQUE INDEX {table}_srcId on {table} (srcId);"""
 
     def __init__(self, conn, table, create=False):
-        super(TransMapGeneLite, self).__init__(conn, table)
+        super(TransMapSrcGeneLite, self).__init__(conn, table)
         if create:
             self.create()
 
@@ -42,7 +42,7 @@ class TransMapGeneLite(HgLiteTable):
         self._create(self.__createSql)
 
     def loads(self, rows):
-        """load rows into table.  Each element of row is a list, tuple, or TransMapGene objects"""
+        """load rows into table.  Each element of row is a list, tuple, or TransMapSrcGene objects"""
         self.inserts(self.__insertSql, rows)
 
     def index(self):
@@ -50,8 +50,48 @@ class TransMapGeneLite(HgLiteTable):
         self.execute(self.__indexSql)
 
 
-def getTransMapSrcAccs(srcDbConn):
-    """get the transmap accession for all source accessions"""
-    alignSrcTbl = PslLite(srcDbConn, SourceDbTables.srcAlignsTbl)
-    return frozenset(alignSrcTbl.queryRows("SELECT qName FROM {table}",
-                                           lambda cur, row: parseTransMapId(row[0])[1]))
+class TransMapSrcXRef(namedtuple("TransMapSrcXRef", ("srcAlnId", "srcId", "accv"))):
+    """link between transmap source ids"""
+    __slots__ = ()
+
+
+class TransMapSrcXRefLite(HgLiteTable):
+    """
+    transmap source id and accession
+    """
+    __createSql = """CREATE TABLE {table} (
+            srcAlnId text not null,
+            srcId text not null,
+            accv text);"""
+    __insertSql = """INSERT INTO {table} (srcAlnId, srcId, accv) VALUES (?, ?, ?);"""
+    __indexSql = ["""CREATE UNIQUE INDEX {table}_srcAlnId on {table} (srcAlnId);""",
+                  """CREATE INDEX {table}_accv on {table} (accv);"""]
+
+    def __init__(self, conn, table, create=False):
+        super(TransMapSrcXRefLite, self).__init__(conn, table)
+        if create:
+            self.create()
+
+    def create(self):
+        """create table"""
+        self._create(self.__createSql)
+
+    def loads(self, rows):
+        """load rows into table.  Each element of row is a list, tuple, or TransMapSrcGene objects"""
+        self.inserts(self.__insertSql, rows)
+
+    def index(self):
+        """create index after loading"""
+        self.executes(self.__indexSql)
+
+    def getSrcIds(self):
+        "get generator over unique source ids"
+        sql = "SELECT distinct(srcId) FROM {table};"
+        for row in self.query(sql):
+            yield row[0]
+
+    def getAccvs(self):
+        "get generator over unique source accv"
+        sql = "SELECT distinct(accv) FROM {table};"
+        for row in self.query(sql):
+            yield row[0]
