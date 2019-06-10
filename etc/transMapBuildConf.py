bd077cbc223ce0d9407cae40a5f5bd8fe1927565
Mark Diekhans <markd@ucsc.edu>
2019-06-10 08:37:29 -0700

    set build to V6

--- etc/transMapBuildConf.py
+++ etc/transMapBuildConf.py
@@ -7,12 +7,12 @@ from transMap.genomeData import ChainType
 
 def getConfig(configPyFile, dataRootDir=None, srcHgDb=None, destHgDb=None,
               annotationType=None, chainType=None):
-    version = "V4"
+    version = "V5"
 
     # Modify versionDir to build in different directory while using
-    # version in file names. 
+    # version in file names.
     versionDir = version
-    
+
     # increment to prevent reusing batch directories on restart
     batchGen = 1
     dataRootDir = os.path.join("/hive/data/inside/transMap", versionDir)
